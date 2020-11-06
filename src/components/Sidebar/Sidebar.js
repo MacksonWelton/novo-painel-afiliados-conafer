a528b21c60a5189d71f554d0d6b282b238bed522
MacksonWelton <mackson.welton@gmail.com>
2020-11-06 08:57:14 -0300

    Alterações de mensagens

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -169,7 +169,7 @@ class Sidebar extends React.Component {
               </DropdownToggle>
               <DropdownMenu className="dropdown-menu-arrow" right>
                 <DropdownItem className="noti-title" header tag="div">
-                  <h6 className="text-overflow m-0">Bem-vindo!</h6>
+                  <h6 className="text-overflow m-0">Bem-vindo(a)!</h6>
                 </DropdownItem>
                 <DropdownItem to="/admin/user-profile" tag={Link}>
                   <i className="ni ni-single-02" />
